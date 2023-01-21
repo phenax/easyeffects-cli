3d1fe89b1fda34ce847ac23dfc188b24f818c82c
Akshay Nair <phenax5@gmail.com>
2023-01-21 12:27:52 +0530

    feat: init commit with cli shit

---
+++ src/main.rkt
@@ -0,0 +1,67 @@
+; #lang racket
+
+(require json)
+(require racket/string)
+
+; TODO: Handle XDG_CONFIG_HOME not set
+(define (get-preset-path source)
+  (string-append* (list
+    (getenv "XDG_CONFIG_HOME")
+    "/easyeffects/input/"
+    source
+    ".json")))
+
+(define (read-source-preset source)
+  (let (
+    [ source-preset-path (get-preset-path source) ]
+  )
+    (string->jsexpr (file->string source-preset-path))))
+
+(define (write-output-preset name preset)
+  (let (
+    [ preset-path (get-preset-path name) ]
+  )
+    (println (jsexpr->string preset))
+    (display-to-file (jsexpr->string preset) preset-path
+      	#:exists 'truncate/replace)
+  ))
+
+(define (hash-path hash path)
+  (let* (
+    ; [ with-default (lambda (v f)
+    ;   (if (eq? v 'null) def (f v))) ]
+
+    [ reducer (lambda (k h) (hash-ref h k)) ]
+
+    [ result (foldl reducer hash path) ]
+  )
+  result))
+
+(define (hash-update-path hash path updater)
+  (match path
+    [ (list)  (updater hash) ]
+    [ (list key tl ...) (hash-update hash key (lambda (val)
+        (hash-update-path val tl updater)
+      )) ]
+  ))
+
+(define output-preset-label "__output__")
+
+(define (main)
+  (let* (
+    [ source-preset (read-source-preset "mic2") ]
+    [ pitch-path (list 'input 'pitch#0 'bypass) ]
+    [ pitch-value (hash-path source-preset pitch-path) ]
+
+    [ updated-preset (hash-update-path source-preset pitch-path (lambda (_) #f)) ]
+
+    [ foobar (hasheq 'foo (hasheq 'a 2)) ]
+  )
+    (write-output-preset output-preset-label updated-preset)
+    (println "Aaayayy donezo")
+    ; (println (hash-path updated-preset (list 'input 'pitch#0)))
+  ))
+
+; Write to output preset json
+; Reload output preset
+
