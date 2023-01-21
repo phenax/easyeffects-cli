3d2069338290cbc9becef67b9c17fb05c741d430
Akshay Nair <phenax5@gmail.com>
2023-01-21 16:46:56 +0530

    feat: adds cli args

--- src/main.rkt
+++ src/main.rkt
@@ -2,7 +2,8 @@
 
 (require json)
 (require racket/string)
- (require racket/system)
+(require racket/system)
+ (require data/maybe)
 
 ; TODO: Handle XDG_CONFIG_HOME not set
 (define (get-preset-path source)
@@ -13,28 +14,19 @@
     ".json")))
 
 (define (read-source-preset source)
-  (let (
-    [ source-preset-path (get-preset-path source) ]
-  )
-    (string->jsexpr (file->string source-preset-path))))
+  (let ([ source-preset-path (get-preset-path source) ])
+  (string->jsexpr (file->string source-preset-path))))
 
 (define (write-output-preset name preset)
-  (let (
-    [ preset-path (get-preset-path name) ]
-  )
-    (display-to-file (jsexpr->string preset) preset-path #:exists 'truncate/replace)
-  ))
+  (let ([ preset-path (get-preset-path name) ])
+  (display-to-file
+    (jsexpr->string preset)
+    preset-path
+    #:exists 'truncate/replace)))
 
 (define (hash-path hash path)
-  (let* (
-    ; [ with-default (lambda (v f)
-    ;   (if (eq? v 'null) def (f v))) ]
-
-    [ reducer (lambda (k h) (hash-ref h k)) ]
-
-    [ result (foldl reducer hash path) ]
-  )
-  result))
+  (let* ([ reducer (lambda (k h) (hash-ref h k)) ])
+  (foldl reducer hash path)))
 
 (define (hash-update-path hash path updater)
   (match path
@@ -46,19 +38,48 @@
 
 (define output-preset-label "__output__")
 
-(define (main)
+(define input-preset (make-parameter output-preset-label))
+(define effect-identifier (make-parameter nothing))
+(define update-property (make-parameter nothing))
+
+(command-line
+  #:program "easyeffects-cli"
+  #:once-each
+  [("-i" "--input-preset") inp
+    "Input preset to use" (input-preset inp)]
+  [("-e" "--effect") eff
+    "Effect identifier" (effect-identifier (just eff))]
+  [("-s"  "--set") key value
+    "Set an effect property" (update-property (just (cons key value)))]
+  [("--enable")
+    "Enable the effect" (update-property (just (cons "bypass" #f)))]
+  [("--disable")
+    "Disable the effect" (update-property (just (cons "bypass" #t)))]
+)
+
+(define (reload-preset preset)
+  (system (string-append "easyeffects -l " preset)))
+
+(define (update-input-effect preset effect update)
   (let* (
-    [ source-preset (read-source-preset "mic2") ]
-    [ pitch-path (list 'input 'pitch#0 'bypass) ]
-    [ pitch-value (hash-path source-preset pitch-path) ]
-    [ updated-preset (hash-update-path source-preset pitch-path (lambda (_) #t)) ]
+    [ source-preset   (read-source-preset preset) ]
+    [ eff-sym         (string->symbol effect) ]
+    [ update-key-sym      (string->symbol (car update)) ]
+    [ pitch-path      (list 'input eff-sym update-key-sym) ]
+    [ to-update-value (lambda (_) (cdr update)) ]
+    [ updated-preset  (hash-update-path source-preset pitch-path to-update-value) ]
   )
-
+    ; (println (hash-path updated-preset (list 'input 'pitch#0)))
     (write-output-preset output-preset-label updated-preset)
-    (system (string-append* (list "easyeffects -l " output-preset-label)))
-    (println "Aaayayy donezo")
-  ))
+    (reload-preset output-preset-label)))
 
-; Write to output preset json
-; Reload output preset
+(define (main)
+  (match (list (input-preset) (effect-identifier) (update-property))
+    [(list preset (just effect) (just update)) #:when (not (eq? preset ""))
+      (update-input-effect preset effect update)
+      (println "Done the deed") ]
+
+    [_ (println "Nothin to do")]))
+
+(main)
 
